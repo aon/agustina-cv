49763f6f0cc04881f6e40ec27646c82a3169f8a3
aon <21188659+aon@users.noreply.github.com>
2024-01-16 13:29:28 -0300

    First upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
 
 export default function Page() {
   return (
-    <main className="container relative mx-auto scroll-my-12 overflow-auto p-4 print:p-12 md:p-16">
+    <main className="container relative mx-auto scroll-my-12 overflow-auto p-4 md:p-16 print:p-12">
       <section className="mx-auto w-full max-w-2xl space-y-8 bg-white print:space-y-6">
         <div className="flex items-center justify-between">
           <div className="flex-1 space-y-1.5">
@@ -93,13 +93,13 @@ export default function Page() {
           </Avatar>
         </div>
         <Section>
-          <h2 className="text-xl font-bold">About</h2>
+          <h2 className="text-xl font-bold">Sobre mí</h2>
           <p className="text-pretty font-mono text-sm text-muted-foreground">
             {RESUME_DATA.summary}
           </p>
         </Section>
         <Section>
-          <h2 className="text-xl font-bold">Work Experience</h2>
+          <h2 className="text-xl font-bold">Experiencia</h2>
           {RESUME_DATA.work.map((work) => {
             return (
               <Card key={work.company}>
@@ -139,7 +139,7 @@ export default function Page() {
           })}
         </Section>
         <Section>
-          <h2 className="text-xl font-bold">Education</h2>
+          <h2 className="text-xl font-bold">Educación</h2>
           {RESUME_DATA.education.map((education) => {
             return (
               <Card key={education.school}>
@@ -159,7 +159,7 @@ export default function Page() {
           })}
         </Section>
         <Section>
-          <h2 className="text-xl font-bold">Skills</h2>
+          <h2 className="text-xl font-bold">Habilidades</h2>
           <div className="flex flex-wrap gap-1">
             {RESUME_DATA.skills.map((skill) => {
               return <Badge key={skill}>{skill}</Badge>;
@@ -168,8 +168,8 @@ export default function Page() {
         </Section>
 
         <Section className="print-force-new-page scroll-mb-16">
-          <h2 className="text-xl font-bold">Projects</h2>
-          <div className="-mx-3 grid grid-cols-1 gap-3 print:grid-cols-3 print:gap-2 md:grid-cols-2 lg:grid-cols-3">
+          <h2 className="text-xl font-bold">Proyectos</h2>
+          <div className="-mx-3 grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3 print:grid-cols-3 print:gap-2">
             {RESUME_DATA.projects.map((project) => {
               return (
                 <ProjectCard
--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -20,111 +20,127 @@ import {
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
-  about:
-    "Full Stack Engineer focused on building products with extra attention to detail",
+  name: "Agustina Staltari",
+  initials: "AA",
+  location: "Mar del Plata, Argentina",
+  locationLink:
+    "https://www.google.com/maps/place/Mar+del+Plata,+Buenos+Aires+Province",
+  about: "Arquitecta",
   summary:
-    "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
+    "Soy Arquitecta, actualmente trabajo en reconocido estudio de Arquitectura en Mar del Plata. A lo largo de mi experiencia descubrí cierto interés en el diseño de interiores. Actualmente me encuentro predispuesta a ampliar mis conocimientos en la materia. Me considero alguien proactiva, con buena predisposición para trabajar en equipo y abierta a nuevas experiencias. Busco crecer tanto personal como profesionalmente.",
+  avatarUrl: "/avatar.jpeg",
   personalWebsiteUrl: "https://jarocki.me",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "agus.staltari@gmail.com",
+    tel: "+5492236333433",
     social: [
-      {
-        name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
-        icon: GitHubIcon,
-      },
+      // {
+      //   name: "GitHub",
+      //   url: "https://github.com/BartoszJarocki",
+      //   icon: GitHubIcon,
+      // },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://www.linkedin.com/in/agustina-staltari-b9596b11b/",
         icon: LinkedInIcon,
       },
-      {
-        name: "X",
-        url: "https://x.com/BartoszJarocki",
-        icon: XIcon,
-      },
+      // {
+      //   name: "X",
+      //   url: "https://x.com/BartoszJarocki",
+      //   icon: XIcon,
+      // },
     ],
   },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
+      school: "Universidad Nacional de Mar del Plata",
+      degree: "Arquitectura",
+      start: "2015",
+      end: "2022",
     },
   ],
   work: [
     {
-      company: "Parabol",
-      link: "https://parabol.co",
-      badges: ["Remote"],
-      title: "Senior Full Stack Developer",
+      company: "Estudio Panasci",
+      link: "https://www.instagram.com/estudiopanasci/?hl=en",
+      badges: ["Full-Time"],
+      title: "Arquitecta",
       logo: ParabolLogo,
-      start: "2021",
-      end: "2024",
-      description:
-        "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
+      start: "2022",
+      end: "Actualidad",
+      description: `- Gestión de múltiples proyectos en simultáneo
+        - Diseño de interiores, mobiliario y decoración
+        - Relación con gremios y proveedores
+        - Supervisión de tareas en obra
+        - Desarrollo de proyectos de construcción y remodelación en viviendas particulares y comercios`,
     },
     {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
-      badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
+      company: "Freelance",
+      // link: "https://clevertech.biz",
+      badges: ["Part-Time"],
+      title: "Arquitecta",
       logo: ClevertechLogo,
-      start: "2015",
+      start: "2021",
+      end: "Actualidad",
+      description: `
+       - Anteproyecto en Mar del Sur, conjunto de viviendas
+       - Diseño de mobiliario
+       - Remodelación viviendas particulares
+       - Confección de planos de Seguridad e Higiene para Comercios`,
+    },
+    {
+      company: "Grupo DAS",
+      link: "hhttps://www.instagram.com/grupodasconstrucciones/?hl=en",
+      badges: ["Full-Time"],
+      title: "Arquitecta",
+      logo: JojoMobileLogo,
+      start: "2021",
+      end: "2022",
+      description:
+        "Renderizado 3D · Modelado 3D · Planos de construcción · AutoCAD",
+    },
+    {
+      company: "DFS Studio",
+      link: "https://www.instagram.com/dfs.estudio/",
+      badges: ["Part-Time"],
+      title: "Arquitecta",
+      logo: NSNLogo,
+      start: "2021",
+      end: "2022",
+      description:
+        "Diseño de folletos · Publicidad · Renderizado · Adobe Photoshop",
+    },
+    {
+      company: "Estudio Roberto Lopez del Hoyo",
+      // link: "https://www.instagram.com/dfs.estudio/",
+      badges: ["Part-Time"],
+      title: "Arquitecta",
+      logo: NSNLogo,
+      start: "2021",
       end: "2021",
       description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
-    },
-    {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
-      badges: [],
-      title: "Android Developer → Lead Android Developer",
-      logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
-      description:
-        "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
-    },
-    {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
-      badges: [],
-      title: "C/C++ Developer",
-      logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Creating and testing software for LTE base stations",
+        "Dibujo técnico · Planos municipales · AutoCAD · SketchUp",
     },
   ],
   skills: [
-    "JavaScript",
-    "TypeScript",
-    "React/Next.js/Remix",
-    "Node.js",
-    "GraphQL",
-    "Relay",
-    "WebRTC",
+    "Gestión de proyectos",
+    "Diseño de interiores",
+    "Mobiliario y decoración",
+    "Dibujo técnico",
+    "Renderizado 3D",
+    "AutoCAD",
+    "Revit",
+    "SketchUp",
+    "Lumion",
   ],
   projects: [
     {
-      title: "Consultly",
+      title: "Sarasa Negro",
       techStack: [
-        "Side Project",
-        "TypeScript",
-        "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
+        "Diseño de interiores",
+        "Dirección de obra",
       ],
-      description: "A platform to build and grow your online business",
+      description: "Restaurant de alto nivel en la ciudad de Mar del Plata",
       logo: ConsultlyLogo,
       link: {
         label: "consultly.com",
@@ -132,10 +148,10 @@ export const RESUME_DATA = {
       },
     },
     {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
+      title: "Muu",
+      techStack: ["Selección de mobiliario"],
       description:
-        "Browser extension that records everything happening in a web application",
+        "Restaurant de carnes",
       logo: MonitoLogo,
       link: {
         label: "monito.dev",
@@ -143,136 +159,52 @@ export const RESUME_DATA = {
       },
     },
     {
-      title: "Jarocki.me",
-      techStack: ["Side Project", "Next.js", "MDX"],
-      description:
-        "My personal website and blog. Built with Next.js and Notion API",
-      logo: JarockiMeLogo,
-      link: {
-        label: "github.com",
-        href: "https://jarocki.me/",
-      },
-    },
-    {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Minimalist calendars, habit trackers and planners generator",
-      logo: Minimal,
-      link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
-      },
-    },
-    {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Generates beautiful wallpapers using random shapes and gradients",
-      logo: BarepapersLogo,
-      link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
-      },
-    },
-    {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
-      link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
-      },
-    },
-    {
-      title: "Parabol",
+      title: "Sarasa Negro",
       techStack: [
-        "Full Stack Developer",
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
+        "Diseño de interiores",
+        "Dirección de obra",
       ],
-      description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
-      logo: ParabolLogo,
+      description: "Restaurant de alto nivel en la ciudad de Mar del Plata",
+      logo: ConsultlyLogo,
       link: {
-        label: "github.com",
-        href: "https://parabol.co/",
+        label: "consultly.com",
+        href: "https://consultly.com/",
       },
     },
     {
-      title: "Evercast",
+      title: "Muu",
+      techStack: ["Selección de mobiliario"],
+      description:
+        "Restaurant de carnes",
+      logo: MonitoLogo,
+      link: {
+        label: "monito.dev",
+        href: "https://monito.dev/",
+      },
+    },
+    {
+      title: "Sarasa Negro",
       techStack: [
-        "Lead Frontend Developer",
-
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
+        "Diseño de interiores",
+        "Dirección de obra",
       ],
-      description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
-      logo: EvercastLogo,
+      description: "Restaurant de alto nivel en la ciudad de Mar del Plata",
+      logo: ConsultlyLogo,
       link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
+        label: "consultly.com",
+        href: "https://consultly.com/",
       },
     },
     {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
+      title: "Muu",
+      techStack: ["Selección de mobiliario"],
       description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
+        "Restaurant de carnes",
+      logo: MonitoLogo,
       link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
+        label: "monito.dev",
+        href: "https://monito.dev/",
       },
-    },
-    {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Howdy is a place for you to join communities you care about",
-      logo: Howdy,
-      link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
-      },
-    },
-    {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
-      link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
-      },
-    },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
     },
   ],
 } as const;
